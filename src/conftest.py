260435b4e054e9e8e2f27c3ba1829d7c500c05a7
Tarik Onalan <onalant@users.noreply.github.com>
2018-08-16 12:49:38 -0700

    Added tests to build step.

--- src/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-def pytest_namespace():
-    print("Do not run pytest in rif project src dir!")
-    print("   location:", __file__)
-    sys.exit(-1)  # Do not run pytest in rif project src dir!
--- src/rif/test/example_test.py
+++
@@ -1,8 +0,0 @@
-import _rif
-
-
-def test_pybind_example():
-    print(_rif.__file__)
-    assert _rif._test._example.add(1, 2) == 3
-    assert _rif._test._example.sub(1, 2) == -1
-    assert _rif._test._example.mul(1, 2) == 2
