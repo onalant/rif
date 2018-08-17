1a899db3b440c53f7129f93e7ebd58bd401b613a
Tarik Onalan <onalant@users.noreply.github.com>
2018-08-17 09:47:56 -0700

    Remove import guards from parsimonious; included in build step.

--- src/rif/sele/sele.py
+++ src/rif/sele/sele.py
@@ -1,48 +1,43 @@
-try:
-    from parsimonious.grammar import Grammar, NodeVisitor
+from parsimonious.grammar import Grammar, NodeVisitor
 
-    # todo: figure out how to properly use parsimonious
-    sele_grammar = Grammar(
-    	"""
-    	expr = (obj ' ')* obj
-    	obj = ray / anameORP
-    	ray = anameORP '->' anameORP
-    	anameORP = anameOR / ('(' anameOR ')')
-    	anameOR = aname ((' OR ' / ' or ') aname)*
-    	aname    = ~'[a-zA-Z0-9]+'
-    	"""
-    )
+# todo: figure out how to properly use parsimonious
+sele_grammar = Grammar(
+    """
+    expr = (obj ' ')* obj
+    obj = ray / anameORP
+    ray = anameORP '->' anameORP
+    anameORP = anameOR / ('(' anameOR ')')
+    anameOR = aname ((' OR ' / ' or ') aname)*
+    aname    = ~'[a-zA-Z0-9]+'
+    """
+)
 
-    class AtomSeleVisitor(NodeVisitor):
-        def __init__(self):
-            self.atom_names = list()
-    
-        def visit_anameOR(self, node, vchild):
-            anames = crappy_visit_anameOR(node)
-            self.atom_names.append(anames)
-    
-        def visit_ray(self, node, vchild):
-            raise ValueError('parsing ray with AtomSeleVisitor')
-    
-        def generic_visit(self, node, vchild):
-            pass
-    
-    class RaySeleVisitor(NodeVisitor):
-        def __init__(self):
-            self.rays_atom_names = list()
-    
-        def visit_ray(self, node, vchild):
-            ray = list()
-            for c in (node.children[0], node.children[2]):
-                ray.append(crappy_visit_anameOR(c.children[0]))
-            self.rays_atom_names.append(tuple(ray))
-    
-        def generic_visit(self, node, vchild):
-            pass
-    
-except ImportError:
-    pass
+class AtomSeleVisitor(NodeVisitor):
+    def __init__(self):
+        self.atom_names = list()
 
+    def visit_anameOR(self, node, vchild):
+        anames = crappy_visit_anameOR(node)
+        self.atom_names.append(anames)
+
+    def visit_ray(self, node, vchild):
+        raise ValueError('parsing ray with AtomSeleVisitor')
+
+    def generic_visit(self, node, vchild):
+        pass
+
+class RaySeleVisitor(NodeVisitor):
+    def __init__(self):
+        self.rays_atom_names = list()
+
+    def visit_ray(self, node, vchild):
+        ray = list()
+        for c in (node.children[0], node.children[2]):
+            ray.append(crappy_visit_anameOR(c.children[0]))
+        self.rays_atom_names.append(tuple(ray))
+
+    def generic_visit(self, node, vchild):
+        pass
 
 def print_node(node):
     print(node)
